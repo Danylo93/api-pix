a1dde9777cebda39c3fc9a4623047d12c202c449
Danylo93 <39104255+Danylo93@users.noreply.github.com>
2022-10-21 08:59:00 -0300

    initial api

---
+++ mercadinho-api/lib/app/entities/user.dart
@@ -0,0 +1,13 @@
+final User{
+  final int? id;
+  final String name;
+  final String email;
+  final String password;
+  User({
+    this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+    
+  });
+}
